fff0ac166fe88a128f18bd6c0f0aab145cabd37d
kyle wong <kyle9535@gmail.com>
2025-02-28 09:48:28 -0800

    good wheel

--- packages/react-frontend/src/pages/Home.jsx
+++ packages/react-frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import CustomWheel from '../components/Wheel';
+import RestaurantModal from '../components/RestaurantModal';
 
 const Home = () => {
   const [mustSpin, setMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [open, setOpen] = useState(false);
   const items = ['Popeyes', 'Santa Cruz Taqueria', 'Jack In The Box', 'Quesedilla Gorilla'];
 
   const handleSpinClick = () => {
@@ -16,14 +18,21 @@ const Home = () => {
   const handleStopSpinning = () => {
     setMustSpin(false);
     setSelectedItem(items[prizeNumber]);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
     <div>
       <h1>Home Page</h1>
-      <CustomWheel mustSpin={mustSpin} prizeNumber={prizeNumber} />
+      <CustomWheel mustSpin={mustSpin} prizeNumber={prizeNumber} onStopSpinning={handleStopSpinning} />
       <button onClick={handleSpinClick}>Spin the Wheel</button>
       {selectedItem && <p>Selected Restaurant: {selectedItem}</p>}
+      
+      <RestaurantModal open={open} handleClose={handleClose} selectedItem={selectedItem} />
     </div>
   );
 };
